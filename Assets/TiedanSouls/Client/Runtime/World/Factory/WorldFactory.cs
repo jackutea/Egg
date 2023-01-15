02dfbe0a15465df3ce0c50fa901c54ebc9d42fbd
杰克有茶 <chenwansal1@163.com>
2023-01-15 20:50:29 +0800

    <refactor> add: WorldFactory-生成角色/技能/HUD/武器

--- Assets/TiedanSouls/Client/Runtime/World/Factory/WorldFactory.cs
+++ Assets/TiedanSouls/Client/Runtime/World/Factory/WorldFactory.cs
@@ -2,18 +2,22 @@ using UnityEngine;
 using TiedanSouls.Asset;
 using TiedanSouls.Infra.Facades;
 using TiedanSouls.World.Entities;
+using TiedanSouls.World.Facades;
 using TiedanSouls.World.Service;
+using TiedanSouls.Template;
 
 namespace TiedanSouls.World {
 
     public class WorldFactory {
 
         InfraContext infraContext;
+        WorldContext worldContext;
 
         public WorldFactory() { }
 
-        public void Inject(InfraContext infraContext) {
+        public void Inject(InfraContext infraContext, WorldContext worldContext) {
             this.infraContext = infraContext;
+            this.worldContext = worldContext;
         }
 
         public FieldEntity CreateFieldEntity() {
@@ -26,6 +30,139 @@ namespace TiedanSouls.World {
             var entity = GameObject.Instantiate(go).GetComponent<FieldEntity>();
             entity.Ctor();
             return entity;
+        }
+
+        public RoleEntity CreateRoleEntity(int typeID, sbyte ally, Vector2 pos) {
+
+            var idService = worldContext.IDService;
+            var templateCore = infraContext.TemplateCore;
+            var assetCore = infraContext.AssetCore;
+
+            // - Entity
+            var worldAssets = assetCore.WorldAssets;
+            bool has = worldAssets.TryGet("entity_role", out GameObject go);
+            if (!has) {
+                TDLog.Error("Failed to get asset: entity_role");
+                return null;
+            }
+
+            var role = GameObject.Instantiate(go).GetComponent<RoleEntity>();
+            role.Ctor();
+
+            // - ID
+            int id = idService.PickRoleID();
+            role.SetID(id);
+
+            // - TM
+            has = templateCore.RoleTemplate.TryGet(typeID, out RoleTM roleTM);
+            if (!has) {
+                TDLog.Error("Failed to get role template: " + typeID);
+                return null;
+            }
+
+            // - Mod
+            var roleModAssets = assetCore.RoleModAssets;
+            has = roleModAssets.TryGet(roleTM.modName, out GameObject roleModPrefab);
+            if (!has) {
+                TDLog.Error("Failed to get sprite: " + roleTM.modName);
+                return null;
+            }
+            GameObject roleMod = GameObject.Instantiate(roleModPrefab, role.Body);
+            role.SetMod(roleMod);
+
+            // - Attr
+            var attrCom = role.AttrCom;
+            attrCom.InitializeHealth(roleTM.hpMax, roleTM.hpMax, roleTM.epMax, roleTM.epMax, roleTM.gpMax, roleTM.gpMax);
+            attrCom.InitializeLocomotion(roleTM.moveSpeed, roleTM.jumpSpeed, roleTM.fallingAcceleration, roleTM.fallingSpeedMax);
+
+            // - Pos
+            role.SetPos(pos);
+
+            // - Ally
+            role.SetAlly(ally);
+
+            // ==== Skillor ====
+            // Dash / BoomMelee / Infinity
+            if (roleTM.skillorTypeIDArray != null) {
+                foreach (var skillorTypeID in roleTM.skillorTypeIDArray) {
+                    CreateSkillor(role, skillorTypeID);
+                }
+            }
+
+            // ==== HUD ====
+            // HpBar
+            var hpBar = CreateHpBarHUD(role.HudSlotCom.HudRoot);
+            role.HudSlotCom.SetHpBarHUD(hpBar);
+
+            // ==== Weapon ====
+            // (Weapon Skillor: Melee / HoldMelee / SpecMelee)
+            CreateWeapon(role, 100);
+
+            return role;
+
+        }
+
+        // ==== Skillor ====
+        void CreateSkillor(RoleEntity role, int typeID) {
+            var idService = worldContext.IDService;
+            var templateCore = infraContext.TemplateCore;
+            int skillorID = idService.PickSkillorID();
+            var skillor = new SkillorModel(skillorID, role);
+            bool has = templateCore.SkillorTemplate.TryGet(typeID, out SkillorTM skillorTM);
+            if (!has) {
+                TDLog.Error("Failed to get skillor template: " + typeID);
+                return;
+            }
+            skillor.FromTM(skillorTM);
+            role.SkillorSlotCom.Add(skillor);
+        }
+
+        // ==== Weapon ====
+        void CreateWeapon(RoleEntity role, int typeID) {
+            var assetCore = infraContext.AssetCore;
+            var templateCore = infraContext.TemplateCore;
+
+            // Weapon TM
+            bool has = templateCore.WeaponTemplate.TryGet(typeID, out WeaponTM weaponTM);
+            if (!has) {
+                TDLog.Error("Failed to get weapon template: " + typeID);
+                return;
+            }
+
+            // Weapon Mod
+            has = assetCore.WeaponModAssets.TryGet(weaponTM.meshName, out GameObject weaponModPrefab);
+            if (!has) {
+                TDLog.Error("Failed to get weapon mod: " + weaponTM.meshName);
+                return;
+            }
+
+            var weapon = new WeaponModel();
+            weapon.weaponType = weaponTM.weaponType;
+            weapon.typeID = weaponTM.typeID;
+            weapon.atk = weaponTM.atk;
+            weapon.def = weaponTM.def;
+            weapon.crit = weaponTM.crit;
+            weapon.skillorMeleeTypeID = weaponTM.skillorMeleeTypeID;
+            weapon.skillorHoldMeleeTypeID = weaponTM.skillorHoldMeleeTypeID;
+            weapon.skillorSpecMeleeTypeID = weaponTM.skillorSpecMeleeTypeID;
+            var weaponMod = GameObject.Instantiate(weaponModPrefab, role.WeaponSlotCom.WeaponRoot);
+            weapon.SetMod(weaponMod);
+            role.WeaponSlotCom.SetWeapon(weapon);
+        }
+
+        // ==== HUD ====
+        // - HpBar
+        public HpBarHUD CreateHpBarHUD(Transform parent) {
+            var assetCore = infraContext.AssetCore;
+            var hudAssets = assetCore.HUDAssets;
+            bool has = hudAssets.TryGet("hud_hp_bar", out GameObject go);
+            if (!has) {
+                TDLog.Error("Failed to get asset: hud_hp_bar");
+                return null;
+            }
+            var hud = GameObject.Instantiate(go, parent).GetComponent<HpBarHUD>();
+            hud.Ctor();
+            return hud;
         }
 
     }
