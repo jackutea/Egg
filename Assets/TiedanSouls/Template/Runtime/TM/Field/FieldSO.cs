e1d9719a4786888fcbaf4e5bcef8c1b0cb7bc76b
zh1668118834 <1668118834@qq.com>
2023-02-18 16:18:00 +0800

    <fix> SO文件脚本引用丢失

--- Assets/TiedanSouls/Template/Runtime/TM/Field/FieldSO.cs
+++ Assets/TiedanSouls/Template/Runtime/TM/Field/FieldSO.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace TiedanSouls.Template {
 
-    [CreateAssetMenu(fileName = "so_field_", menuName = "TiedanSouls/Template/FieldSo", order = 0)]
+    [CreateAssetMenu(fileName = "so_field_", menuName = "TiedanSouls/Template/FieldSO", order = 0)]
     public class FieldSO : ScriptableObject {
 
         public FieldTM tm;
